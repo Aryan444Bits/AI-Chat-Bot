f774290d6896e39a2b871a9ee95ca9610a270450
Aryan444Bits <branwalaryan71@gmail.com>
2025-10-05 02:34:51 +0530

    deploying

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const cookieParser = require('cookie-parser');
 const express = require('express')
 const cors = require('cors');
+const path = require('path');
 
 /*Routes*/
 const authRoutes = require("./routes/auth.routes")
@@ -18,10 +19,17 @@ app.use(cors({
 /*Using middlwares */
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,'../public')));
+
 
 
 /*Using Routes */
 app.use('/api/auth',authRoutes)
 app.use('/api/chat',chatRoutes)
+
+
+app.get('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
 module.exports = app;
